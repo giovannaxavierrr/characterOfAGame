89edf34c6a2557081416753ed09765d70fd8e1fc
Giovanna Xavier <xaviergiovanna234@gmail.com>
2025-07-15 10:12:27 -0300

    Programa mais elaborado

--- Metricas.java
+++ Metricas.java
@@ -1,46 +1,72 @@
+import java.util.Scanner;
+
 public class Metricas {
     
-    String name, classe;
-    int nivel, vida, forca, mana;
+    Scanner scanner = new Scanner(System.in);
+    String name, nomeClasse;
+    int escolhido, classe, nivel, vida, forca, mana;
+    int danoLevado, verificar;
+
+    public void escolherClasse() {
+        System.out.println("Personagens: ");
+        System.out.println("1 - Mago\t|\t2 - Guerreiro\t|\t3 - Arqueiro");
+        System.out.print("Escolha sua opcao: ");
+        classe = scanner.nextInt();
+        if(classe <= 0 || classe > 3) {
+            System.out.println("Invalido, tente novamente");
+            System.out.print("Escolha sua opcao (de 1 a 3): ");
+            classe = scanner.nextInt();
+        }
+    }
+    
+    public void choice() {
+        System.out.println("Vez do jogador "+name);
+        System.out.println("Qual seu proximo passo?\n1 - Atacar (golpe comum)\t|\t2 - Usar habilidade especial");
+        escolhido = scanner.nextInt();
+    }
 
     public void atacar() {
         
-        if(classe.equalsIgnoreCase("mago")) {
+        if(classe == 1) {
             System.out.println(name+" lanca feitico! Causando 30 de dano.");
             mana -= 10;
-        } else if(classe.equalsIgnoreCase("guerreiro")) {
-            System.out.println(name+"Guerreiro ataca com sua espada! Causando 30 de dano.");
+        } else if(classe == 2) {
+            System.out.println(name+" ataca com sua espada! Causando 30 de dano.");
             forca -= 10;
 
-        } else if(classe.equalsIgnoreCase("arqueiro")) {
+        } else if(classe == 3) {
             System.out.println(name+" lanca multiplas flechas! Causando 30 de dano.");
             forca -= 10;
         }
     }
 
     public void receberDano() { //reduz a vida do personagem
-        int danoLevado = vida - 30;
+        danoLevado = vida - 30;
         if(danoLevado > 0) {
             vida = danoLevado;
             System.out.println(name+" levou 30 de dano. Vida atual: "+vida);
         } else {
             System.out.println(name+" foi derrotado!");
+            vida = 0;
         }
 
     }
 
     public void usarHabilidadeEspecial() { //diferente para cada classe, ex: mago lança feitico, guerreiro usa golpe poderoso
 
-        if(classe.equalsIgnoreCase("mago")) {
+        if(classe == 1) {
             System.out.println(name+" usa habilidade especial e lanca feitico atordoante!");
             mana -= 20;
-            vida += 5;
-            System.out.println("+5 vida. Vida atual: "+vida+("\n-20 mana. Mana atual: "+mana));
-        } else if(classe.equalsIgnoreCase("guerreiro")) {
+            if(vida <= 95) {
+                vida += 5;
+                System.out.print("+5 vida. ");
+            }
+            System.out.println("Vida atual: "+vida+("\n-20 mana. Mana atual: "+mana));
+        } else if(classe == 2) {
             System.out.println(name+" usa habilidade especial e da um golpe poderoso!");
             forca -= 30;
-            System.out.println("-30 força. Forca atual: "+forca);
-        } else if(classe.equalsIgnoreCase("arqueiro")) {
+            System.out.println("-30 forca. Forca atual: "+forca);
+        } else if(classe == 3) {
             System.out.println(name+" usa habilidade especial e lanca multiplas flechas!");
             forca -= 30;
             System.out.println("-30 força. Forca atual: "+forca);
@@ -49,20 +75,27 @@ public class Metricas {
 
     public void receberDanoEspecial() { //reduz a vida do personagem após ser atingido com habilidade especial
 
-        int danoLevado = vida - 40;
+        danoLevado = vida - 40;
         if(danoLevado > 0) {
             vida = danoLevado;
             System.out.println(name+" levou 40 de dano. Vida atual: "+danoLevado);
         } else {
             System.out.println(name+" foi derrotado!");
+            vida = 0;
         }
 
     }
 
     public void mostrarStatus() {
 
-        System.out.print("Status: \nNome: "+name+" | Classe: "+classe+" | Nivel: "+nivel+" | Vida: "+vida+" | Forca: "+forca);
-        if(classe.equalsIgnoreCase("mago")) {
+        switch(classe) {
+            case 1: nomeClasse = "Mago"; break;
+            case 2: nomeClasse = "Guerreiro"; break;
+            case 3: nomeClasse = "Arqueiro"; break;
+            default: nomeClasse = "Classe desconhecida";
+        }
+        System.out.print("Status: \nNome: "+name+" | Classe: "+nomeClasse+" | Nivel: "+nivel+" | Vida: "+vida+" | Forca: "+forca);
+        if(classe == 1) {
             System.out.println(" | Mana: "+mana); 
         }
         System.out.println();
@@ -77,16 +110,16 @@ public class Metricas {
             System.out.println("Recompensa: +5 vida!");
         }
         forca += 5;
-        if(classe.equalsIgnoreCase("mago") && mana < 100) {
+        if(classe == 1 && mana < 100) {
             mana +=10;
             System.out.println("Recompensa: +10 mana!");
         }
-        
-
+        System.out.println();
     }
 
     public void desenharPersonagem() {
-        if(classe.equalsIgnoreCase("mago")) {
+
+        if(classe == 1) {
             System.out.println("                                                   \r\n" + //
                                 "                                                   \r\n" + //
                                 "                         -######   -               \r\n" + //
@@ -115,7 +148,7 @@ public class Metricas {
                                 "                                                   \r\n" + //
                                 "                                                   ");
 
-        } else if(classe.equalsIgnoreCase("guerreiro")) {
+        } else if(classe == 2) {
             System.out.println("                                                    \r\n" + //
                                 "                      .-....-.                     \r\n" + //
                                 "                    --        ..                   \r\n" + //
@@ -169,5 +202,4 @@ public class Metricas {
                                 "                                                   ");
         }
     }
-
 }
--- Personagem.java
+++ Personagem.java
@@ -1,39 +1,104 @@
+import java.util.Scanner;
+
 public class Personagem {
 
     public static void main (String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String nomeClasse;
+        int vez = 1;
+        int verificar, rodada = 0;
+        boolean estaJogando = true;
+
         //personagem 1
         Metricas personagem1 = new Metricas(); 
-        personagem1.name = "Giovanna";
-        personagem1.classe = "Mago"; //guerreiro, mago ou arqueiro
+        System.out.print("Digite o nome do primeiro personagem: ");
+        personagem1.name = scanner.nextLine();
+        personagem1.escolherClasse();
         personagem1.nivel = 1; //inicia em 1
         personagem1.vida = 100; //max 100
         personagem1.mana = 70; //somente para a classe mago, max 100
         personagem1.forca = 50;
+        switch(personagem1.classe) {
+            case 1: nomeClasse = "Mago"; break;
+            case 2: nomeClasse = "Guerreiro"; break;
+            case 3: nomeClasse = "Arqueiro"; break;
+            default: nomeClasse = "Classe desconhecida";
+        }
         //personagem 2
         Metricas personagem2 = new Metricas(); 
-        personagem2.name = "Sabrina";
-        personagem2.classe = "Arqueiro"; //guerreiro, mago ou arqueiro
+        System.out.print("Digite o nome do segundo personagem: ");
+        personagem2.name = scanner.nextLine();
+        personagem2.escolherClasse();
         personagem2.nivel = 1; //inicia em 1
         personagem2.vida = 100; //max 100
         personagem2.forca = 70;
+        switch(personagem2.classe) {
+            case 1: nomeClasse = "Mago"; break;
+            case 2: nomeClasse = "Guerreiro"; break;
+            case 3: nomeClasse = "Arqueiro"; break;
+            default: nomeClasse = "Classe desconhecida";
+        }
 
         //jogo rolando
-        System.out.println("Heroi criado: "+personagem1.name+" ("+personagem1.classe+")");
-        personagem1.desenharPersonagem();
-        System.out.println("Heroi criado: "+personagem2.name+" ("+personagem2.classe+")");
-        personagem2.desenharPersonagem();
-        personagem1.atacar();
-        personagem2.receberDano();
-        personagem2.atacar();
-        personagem1.receberDano();
-        personagem1.usarHabilidadeEspecial();
-        personagem2.receberDanoEspecial();
-        personagem1.subirNivel();
         personagem1.mostrarStatus();
+        personagem1.desenharPersonagem();
         personagem2.mostrarStatus();
-        personagem1.usarHabilidadeEspecial();
-        personagem2.receberDanoEspecial();
+        personagem2.desenharPersonagem();
+        
+
+        while(estaJogando) {
+        
+            personagem1.choice();
+            System.out.println();
+            if(personagem1.escolhido == 1) {
+                personagem1.atacar();
+                personagem2.receberDano();
+            } else if(personagem1.escolhido == 2) {
+                personagem1.usarHabilidadeEspecial();
+                personagem2.receberDanoEspecial();
+            }
+            if(personagem2.vida <= 0) {
+                System.out.println(personagem1.name+" IS THE CHAMPIONNN!");
+                break;
+            }
+            System.out.println();
+            personagem2.choice();
+            System.out.println();
+            if(personagem2.escolhido == 1) {
+                personagem2.atacar();
+                personagem1.receberDano();
+            } else if(personagem2.escolhido == 2) {
+                personagem2.usarHabilidadeEspecial();
+                personagem1.receberDanoEspecial();
+                
+            }
+            if(personagem2.vida <= 0) {
+                System.out.println(personagem2.name+" IS THE CHAMPIONNN!");
+                break;
+            }
+            System.out.println();
+            System.out.println("Verificar o status de cada jogador?\n1 - Sim\n2 - Nao, continuar jogo");
+            verificar = scanner.nextInt();
+            System.out.println();
+            if (verificar == 1) {
+                personagem1.mostrarStatus();
+                personagem2.mostrarStatus();
+            } else if (verificar == 2) {
+                System.out.println("Continuando jogo...");
+            } else {
+                System.out.println("Invalido, selecione a opção 1 ou 2.");
+                System.out.println("Verificar o status de cada jogador?\n1 - Sim\n2 - Nao, continuar jogo");
+                verificar = scanner.nextInt();
+            }
+            System.out.println();
+            rodada++;
+            if(rodada == 3 || rodada == 5) {
+                personagem1.subirNivel();
+            } else if (rodada == 2 || rodada == 4) {
+                personagem2.subirNivel();
+            }
+        }
 
     }
 }
